893ec249e0124003411d8726e13241e60a00a3bb
Paras <paraskamliya5@gmail.com>
2023-11-11 15:36:57 +0530

    BE update

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
-const { Configuration, OpenAIApi } = require("openai");
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+const { OpenAIAPI, OpenAIAPIConfiguration } = require("openai");
+require('dotenv').config();
+
+const config = new OpenAIAPIConfiguration({
+    apiKey: process.env.OPEN_AI_KEY
 });
-const openai = new OpenAIApi(configuration);
+
+const openai = new OpenAIAPI(config);
+// const { Configuration, OpenAIApi } = require("openai");
+// const configuration = new Configuration({
+//     apiKey: process.env.OPEN_AI_KEY,
+// });
+// const openai = new OpenAIApi(configuration);
 
 app.post("/start", async (req, res) => {
     try {
@@ -19,11 +27,31 @@ app.post("/start", async (req, res) => {
             ],
             max_tokens: 100
         });
-        console.log(description.data.choices[0].content);
+        console.log(description.choices[0].message.content);
         res.send("Hi");
     } catch (error) {
         res.status(400).send(error);
     }
+    // let API_URL = "https://api.openai.com/v1/chat/completions";
+    // const API_KEY = process.env.OPEN_AI_KEY;
+    // try {
+    //     const response = await fetch(API_URL, {
+    //         method: "POST",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //             Authorization: `Bearer ${API_KEY}`,
+    //         },
+    //         body: JSON.stringify({
+    //             model: "gpt-3.5-turbo",
+    //             messages: [{ role: "user", content: req.body.content }],
+    //         }),
+    //     });
+    //     const data = await response.json();
+    //     console.log(data)
+    //     res.send(data);
+    // } catch (error) {
+    //     console.error("Error:", error);
+    // }
 });
 
 app.listen(8000, () => {
