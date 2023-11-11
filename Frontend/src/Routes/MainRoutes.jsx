fb5b9db836353102fa44a6c0ee36c3f862d58637
Jai <jayachandran9629040900@gmail.com>
2023-11-11 20:00:55 +0530

    HomePage Updated!

--- Frontend/src/Routes/MainRoutes.jsx
+++ Frontend/src/Routes/MainRoutes.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
-import Courses from '../Pages/Course'
-import Dashboard from '../Pages/Dashboard'
+import Courses from '../Pages/Dashboard'
+import Dashboard from '../Pages/Courses'
 import InterviewPage from '../Pages/InterviewPage'
+import HomePage from '../Pages/HomePage'
 
 const MainRoutes = () => {
   return (
     <>
         <Routes>
-            <Route path="/" element={<Dashboard />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/courses" element={<Courses />} />
             <Route path="/interview" element={<InterviewPage />} />
         </Routes>
